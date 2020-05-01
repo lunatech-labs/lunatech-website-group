3ccc28e1770313b194803ab4e5b7d2ade4273a66
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2020-05-01 14:41:55 +0200

    Display latest post image with title and link

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
@@ -8,13 +8,11 @@ def get_post_embed():
     request = requests.get('https://life.lunatech.com/wp-json/wp/v2/posts/?context=embed&per_page=1')
     post = request.json()[0]
     return {'link': post['link'], 'title': post['title']['rendered'], 'featured_media': post['featured_media'], 'featured_media_url':  post['jetpack_featured_media_url']}
-    
-
 
 @app.route('/')
 def home():
     pe = get_post_embed()
-    return pe
+    return render_template('home.html', title=pe['title'], link=pe['link'], image=pe['featured_media_url'])
 
 if __name__ == "__main__":
     app.run()
